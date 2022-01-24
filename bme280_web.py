ed5feaea74937929fcc48be1d4f4004f03efa190
Tom Øyvind Hogstad <tom.oyvind.hogstad@gmail.com>
2022-01-24 11:19:31 +0100

    Ryddet i HTML kode i bme_web.py

--- bme280_web.py
+++ bme280_web.py
@@ -37,7 +37,7 @@ if not station.isconnected():
     print('Connection successful')
     print(station.ifconfig())
 else:
-    print('Already is connected!')
+    print('Already connected!')
     print(station.ifconfig())
     
 
@@ -47,20 +47,32 @@ else:
 def web_page():
   bme = BME280.BME280(i2c=i2c)
   
-  html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,"><style>body { text-align: center; font-family: "Trebuchet MS", Arial;}
-  table { border-collapse: collapse; width:35%; margin-left:auto; margin-right:auto; }
-  th { padding: 12px; background-color: #0043af; color: white; }
-  tr { border: 1px solid #ddd; padding: 12px; }
-  tr:hover { background-color: #bcbcbc; }
-  td { border: none; padding: 12px; }
-  .sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px;
-  </style></head><body><h1>ESP with BME280</h1>
-  <table><tr><th>MEASUREMENT</th><th>VALUE</th></tr>
-  <tr><td>Temp. Celsius</td><td><span class="sensor">""" + str(bme.temperature) + """</span></td></tr>
-  <tr><td>Temp. Fahrenheit</td><td><span class="sensor">""" + str(round((bme.read_temperature()/100.0) * (9/5) + 32, 2))  + """F</span></td></tr>
-  <tr><td>Pressure</td><td><span class="sensor">""" + str(bme.pressure) + """</span></td></tr>
-  <tr><td>Humidity</td><td><span class="sensor">""" + str(bme.humidity) + """</span></td></tr></body></html>"""
+  html = """
+    <html>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="icon" href="data:,">
+            <style>
+                body { text-align: center; font-family: "Trebuchet MS", Arial;}
+                table { border-collapse: collapse; width:35%; margin-left:auto; margin-right:auto; }
+                th { padding: 12px; background-color: #0043af; color: white; }
+                tr { border: 1px solid #ddd; padding: 12px; }
+                tr:hover { background-color: #bcbcbc; }
+                td { border: none; padding: 12px; }
+                .sensor { color:white; font-weight: bold; background-color: #bcbcbc; padding: 1px;
+            </style>
+        </head>
+        <body>
+            <h1>ESP with BME280</h1>
+            <table>
+            <tr><th>MEASUREMENT</th><th>VALUE</th></tr>
+            <tr><td>Temp. Celsius</td><td><span class="sensor">""" + str(bme.temperature) + """</span></td></tr>
+            <tr><td>Temp. Fahrenheit</td><td><span class="sensor">""" + str(round((bme.read_temperature()/100.0) * (9/5) + 32, 2))  + """F</span></td></tr>
+            <tr><td>Pressure</td><td><span class="sensor">""" + str(bme.pressure) + """</span></td></tr>
+            <tr><td>Humidity</td><td><span class="sensor">""" + str(bme.humidity) + """</span></td></tr>
+            </table>
+        </body>
+    </html>"""
   return html
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
